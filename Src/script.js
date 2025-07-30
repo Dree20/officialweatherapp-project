a52665abc8955c2f2793ad4c50dccb19c71445ea
Dree20 <dreannaad@gmail.com>
2025-07-30 18:16:47 -0400

    Updated html and JS

--- Src/script.js
+++ Src/script.js
@@ -1,1 +1,6 @@
-alert("Hello World!");
+function changeSubmit(event) {
+  event.preventDefault();
+  alert("searching");
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", changeSubmit);
