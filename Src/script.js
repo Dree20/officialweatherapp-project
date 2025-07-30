6b90711eca34c91ab155429836084f01007ceee2
Dree20 <dreannaad@gmail.com>
2025-07-30 18:31:05 -0400

    Testing JS

--- Src/script.js
+++ Src/script.js
@@ -1,6 +1,1 @@
-function searchChange(event) {
-  event.preventDefault();
-  alert("searching");
-}
 let inputForm = document.querySelector("#search-form");
-inputForm.addEventListener("submit", searchChange);
