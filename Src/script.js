fdd2a0ad290da22a54b8b241d2fa8658ab01e5de
Dree20 <dreannaad@gmail.com>
2025-07-31 13:09:45 -0400

    Updated html and Css

--- Src/script.js
+++ Src/script.js
@@ -1,6 +1,37 @@
 function changeInput(event) {
   event.preventDefault();
-  alert("searching");
+  let cityChange = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = cityChange.value;
 }
-let inputForm = document.querySelector("#search-form");
-inputForm.addEventListener("sumbit", changeInput);
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0 ${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", changeInput);
+
+let currentDateElement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateElement.innerHTML = formatDate(currentDate);
